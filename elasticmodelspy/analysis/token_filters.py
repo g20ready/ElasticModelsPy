3e9016888d8884a7d4559f39c7c7a9e1d089a9e5
giannis <giannis@giaola.com>
2017-11-04 21:39:14 +0200

    Added all Token Filters for Analysis excepted the below: 1)Keyword Repeat Token Filter2)Snowball Token Filter3)Phonetic Token Filter4)Shingle Token Filter5)CJK Width Token Filter6)CJK Bigram Token FilterRemained to be add: 1)Shingle Token Filter.

--- elasticmodelspy/analysis/token_filters.py
+++ elasticmodelspy/analysis/token_filters.py
@@ -85,7 +85,7 @@ class NgramTokenFilter(TokenFilter):
         self.min_gram = min_gram
         self.max_gram = max_gram
 
-    def __filter_data__(self):
+    def _serialize_token_filter_data(self):
         return dict(
             min_gram=self.min_gram,
             max_gram=self.max_gram
@@ -99,7 +99,7 @@ class EdgeNgramTokenFilter(TokenFilter):
         self.min_gram = min_gram
         self.max_gram = max_gram
 
-    def __filter_data__(self):
+    def _serialize_token_filter_data(self):
         return dict(
             min_gram=self.min_gram,
             max_gram=self.max_gram,
@@ -116,7 +116,7 @@ class StopTokenFilter(TokenFilter):
         self.ignore_case = ignore_case
         self.remove_trailing = remove_trailing
 
-    def __filter_data__(self):
+    def _serialize_token_filter_data(self):
         return dict(
             stopwords = self.stopwords,
             stopwords_path = self.stopwords_path,
@@ -126,8 +126,9 @@ class StopTokenFilter(TokenFilter):
 
 
 class BaseWordDelimiterTokenFilter(TokenFilter):
-    def __init__(self, name, type, protected_words, generate_word_parts=True, generate_number_parts=True,
-                 catenate_words=False, catenate_numbers=False, catenate_all=False,
+    def __init__(self, name, type, protected_words, generate_word_parts=True,
+                 generate_number_parts=True, catenate_words=False,
+                 catenate_numbers=False, catenate_all=False,
                  split_on_case_change=True, preserve_original =False,
                  split_on_numerics=True, stem_english_possessive=True,
                  protected_words_path='config/' ):
@@ -150,7 +151,7 @@ class BaseWordDelimiterTokenFilter(TokenFilter):
         else:
             self.protected_words_path = protected_words_path
 
-    def __filter_data__(self):
+    def _serialize_token_filter_data(self):
         return dict(
             generate_word_parts = self.generate_word_parts,
             generate_number_parts = self.generate_number_parts,
@@ -179,16 +180,253 @@ class WordDelimeterGraphTokenFilter(BaseWordDelimiterTokenFilter):
                                                             catenate_all=True, **kwargs)
 
 
+class StemmerTokenFilter(TokenFilter):
+    def __init__(self, name , language):
+        super(StemmerTokenFilter, self).__init__(name, 'stemmer')
+        self.languague = language
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            language = self.languague
+        )
+
+
+class StemmerOverrideTokenFilter(TokenFilter):
+    def __init__(self, name , rules, rules_path):
+        super(StemmerOverrideTokenFilter, self).__init__(name, 'stemmer_override')
+
+        if rules_path:
+            self.rules = []
+            self.rules_path = rules_path
+        else:
+            self.rules = rules
+            self.rules_path = u''
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            rules = self.rules,
+            rules_path = self.rules_path
+        )
+
+
+class KeywordMarkerTokenFilter(TokenFilter):
+    def __init__(self,name, keywords , keywords_path, keywords_pattern, ignore_case=False):
+        super(KeywordMarkerTokenFilter, self).__init__(name, 'keyword_marker')
+
+        if keywords_path:
+            self.keywords = []
+            self.keywords_path = keywords_path
+        else:
+            self.keywords = keywords
+            self.keywords_path = u''
+        self.keywords_pattern = keywords_pattern
+        self.ignore_case = ignore_case
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            keywords = self.keywords,
+            keywords_path = self.keywords_path,
+            keywords_pattern = self.keywords_pattern,
+            ignore_case = self.ignore_case
+        )
+
+class KStemTokenFiltrer(TokenFilter):
+    def __init__(self, name):
+        super(KStemTokenFiltrer, self).__init__(name, 'kstem')
+
+
 class SynonymTokenFilter(TokenFilter):
-    def __init__(self, name, synonyms_path='analysis/synonym.txt', tokenizer='whitespace', ignore_case=False):
+    def __init__(self, name, synonyms_path, tokenizer='whitespace', ignore_case=False):
         super(SynonymTokenFilter, self).__init__(name, 'synonym')
         self.synonyms_path = synonyms_path
         self.tokenizer = tokenizer
         self.ignore_case = ignore_case
 
-    def __filter_data__(self):
+    def _serialize_token_filter_data(self):
         return dict(
             synonyms_path = self.synonyms_path,
             tokenizer = self.tokenizer,
             ignore_case = self.ignore_case
+        )
+
+class ReverseTokenFiltrer(TokenFilter):
+    def __init__(self, name):
+        super(ReverseTokenFiltrer, self).__init__(name, 'reverse')
+
+
+class ElisionTokenFilter(TokenFilter):
+    def __init__(self, name, articles):
+        super(ElisionTokenFilter, self).__init__(name, 'elision')
+        self.articles = articles
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            articles = self.articles,
+        )
+
+
+class TruncateTokenFilter(TokenFilter):
+    def __init__(self, name, length=10):
+        super(TruncateTokenFilter, self).__init__(name, 'truncate')
+        self.length = length
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            length = self.length
+        )
+
+
+class UniqueTokenFilters(TokenFilter):
+    def __init__(self, name, only_on_same_position=False):
+        super(UniqueTokenFilters, self).__init__(name, 'unique')
+        self.only_on_same_position = only_on_same_position
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            only_on_same_position = self.only_on_same_position
+        )
+
+
+class PatternCaptureTokenFilter(TokenFilter):
+    def __init__(self, name, patterns, preserve_original=True):
+        super(PatternCaptureTokenFilter, self).__init__(name, 'pattern_capture')
+        self.patterns = patterns
+        self.preserve_original = preserve_original
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            patterns = self.patterns,
+            preserve_original = self.preserve_original
+        )
+
+
+class PatternReplaceTokenFilter(TokenFilter):
+    def __init__(self, name, pattern, replacement=None):
+        super(PatternReplaceTokenFilter, self).__init__(name, 'pattern_replace')
+        self.pattern = pattern,
+        self.replacement = replacement
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            pattern = self.pattern,
+            replacement = self.replacement or u''
+        )
+
+
+class TrimTokenFilter(TokenFilter):
+    def __init__(self, name):
+        super(TrimTokenFilter, self).__init__(name, 'trim')
+
+
+class LimitTokenCountTokenFilter(TokenFilter):
+    def __init__(self, name, max_token_count=1, consume_all_tokens=False):
+        super(LimitTokenCountTokenFilter, self).__init__(name, 'limit')
+        self.max_token_count = max_token_count
+        self.consume_all_tokens = consume_all_tokens
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            max_token_count = self.max_token_count,
+            consume_all_tokens = self.consume_all_tokens
+        )
+
+class CommonGramsTokenFilter(TokenFilter):
+    def __init__(self, name, common_words, common_words_path='config/',
+                 ignore_case=False, query_mode=False):
+        super(CommonGramsTokenFilter, self).__init__(name, 'common_grams')
+        self.common_words = common_words
+        self.common_words_path = common_words_path
+        self.ignore_case = ignore_case
+        self.query_mode = query_mode
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            common_words = self.common_words,
+            common_words_path = self.common_words_path,
+            ignore_case = self.ignore_case,
+            query_mode = self.query_mode
+        )
+
+
+class DelimitedPayloadTokenFilter(TokenFilter):
+    def __init__(self, name, delimiter='|', encoding='float'):
+        super(DelimitedPayloadTokenFilter, self).__init__(name, 'delimited_payload_filter')
+        self.delimiter = delimiter
+        self.encoding = encoding
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            delimiter = self.delimiter,
+            encoding = self.encoding
+        )
+
+class KeepWordsTokenFilter(TokenFilter):
+    def __init__(self, name, keep_words, keep_words_path, keep_words_case=False):
+        super(KeepWordsTokenFilter, self).__init__(name, 'keep')
+        self.keep_words = keep_words
+        self.keep_words_path = keep_words_path
+        self.keep_words_case = keep_words_case
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            keep_words = self.keep_words,
+            keep_words_path = self.keep_words_path,
+            keep_words_case = self.keep_words_case
+        )
+
+
+class KeepTypesTokenFilter(TokenFilter):
+    def __init__(self, name, types):
+        super(KeepTypesTokenFilter, self).__init__(name, 'keep_types')
+        self.types = types
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            types = self.types,
+        )
+
+class ClassicTokenFilter(TokenFilter):
+    def __init__(self, name):
+        super(ClassicTokenFilter, self).__init__(name, 'classic')
+
+
+class ApostropheTokenFilter(TokenFilter):
+    def __init__(self, name):
+        super(ApostropheTokenFilter, self).__init__(name, 'apostrophe')
+
+
+class DecimalDigitTokenFilter(TokenFilter):
+    def __init__(self, name):
+        super(DecimalDigitTokenFilter, self).__init__(name, 'decimal_digit')
+
+
+class FingerprintTokenFilter(TokenFilter):
+    def __init__(self, name, separator= u' ', max_output_size=255):
+        super(FingerprintTokenFilter, self).__init__(name, 'fingerprint')
+        self.separator = separator
+        self.max_output_size= max_output_size
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            separator = self.separator,
+            max_output_size = self.max_output_size
+        )
+
+
+class MinhashTokenFilter(TokenFilter):
+    def __init__(self, name, hash_count=1, bucket_count=512,
+                 hash_set_size=1):
+        super(MinhashTokenFilter, self).__init__(name, 'min_hash')
+        self.hash_count = hash_count
+        self.bucket_count= bucket_count
+        self.hash_set_size= hash_set_size
+        self.with_rotation= True if self.hash_set_size == 1 else False
+
+
+    def _serialize_token_filter_data(self):
+        return dict(
+            hash_count = self.hash_count,
+            bucket_count = self.bucket_count,
+            hash_set_size = self.hash_set_size,
+            with_rotation = self.with_rotation
         )
