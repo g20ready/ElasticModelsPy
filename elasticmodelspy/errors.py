593da7587b2687ff568eef34832e1ba51920cefd
Marsel Tzatzo <tmarselg@gmail.com>
2017-11-13 22:46:24 +0200

    Adds base error class

---
+++ elasticmodelspy/errors.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Created by Marsel Tzatzo on 13/11/2017.
+"""
+class Error(Exception):
+    """Base-class for all exceptions raised by elasticmodelspy."""
