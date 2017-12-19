862d29af5ebd0e86b22b5cf99ae5dde4c5ddb57f
Marsel Tzatzo <tmarselg@gmail.com>
2017-12-19 21:38:06 +0200

    Adds import statements for analysis

--- elasticmodelspy/analysis/__init__.py
+++ elasticmodelspy/analysis/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .analyzers import Analyzer, StandardAnalyzer, SimpleAnalyzer, WhiteSpaceAnalyzer, StopAnalyzer, \
+    KeywordAnalyzer, PatternAnalyzer, CustomAnalyzer
 from .tokenizers import Tokenizer, StandardTokenizer, EdgeNGramTokenizer, NGramTokenizer
 from .token_filters import TokenFilter, LanguageTokenFilter, StopwordsTokenFilter
 
